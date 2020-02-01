2d33400db7e4a3268877f3663633c0179568ef32
Stephen Green <stephengreen1028@gmail.com>
2020-02-01 13:45:48 -0700

    Started on the orm.js file, switched the server.js file to using the old body-parser package as I found a bug I couldn't fix without using body-parser

--- server.js
+++ server.js
@@ -1,32 +1,21 @@
-// Require in necessary dependencies
-var express = require('express');
-var bodyParser = require('body-parser');
-var exphbs = require('express-handlebars');
+var express = require("express");
+var bodyParser = require("body-parser");
+var exphbs = require("express-handlebars");
 
 var app = express();
-// PORT set up
 var PORT = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.engine('handlebars', exphbs({
-    defaultLayout: "main"
-}));
-app.set('view engine', 'handlebars');
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Routes
-var routes = require('./controllers/burgers_controllers');
-
+var routes = require("./controllers/burgers_controller.js");
 app.use(routes);
 
-// PORT listener
-app.listen(PORT, function () {
-    console.log('Server listening on: http://localhost:' + PORT)
+app.listen(PORT, function() {
+  console.log("Server listening on: http://localhost:" + PORT);
 });
-
-
